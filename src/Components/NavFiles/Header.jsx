4c0ad581ba931dc763b605fe420dc3933f5e92dd
Seba <122824272+SebaHel@users.noreply.github.com>
2024-10-11 00:30:58 +0200

    Added Mobile Responsivness css main page

--- src/Components/NavFiles/Header.jsx
+++ src/Components/NavFiles/Header.jsx
@@ -2,8 +2,10 @@ import { Link } from 'react-router-dom'
 
 import classes from './Header.module.css'
 import NavList from './NavLists'
+import { useState } from 'react';
 
 export default function Header(){
+    const [isNavOpen, setIsNavOpen] = useState(false);
     const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
     const currentDayindex = new Date().getDay();
@@ -11,12 +13,21 @@ export default function Header(){
     const currentDayName = dayNames[currentDayindex];
 
 
+    const toggleNav =() =>{
+        setIsNavOpen(!isNavOpen);
+    }
+
     return(
         
 
         <header className={classes.header}>
             <Link to="/" className={classes.link}><span className="material-symbols-outlined" style={{fontSize:"40px", justifyContent:'center',alignContent:'center'}}>videocam </span>CINEMA</Link>
-            <div className={classes.nav}>
+            <button className={classes.menuIcon} onClick={toggleNav}>
+              <span className="material-symbols-outlined" style={{ fontSize: '36px' }}>
+                {isNavOpen ? 'close' : 'menu'}
+              </span>
+            </button>
+            <div className={`${classes.nav} ${isNavOpen ? classes.open : ''}`}>
                 <Link to={`/repertoire/${currentDayName}`} className={classes.link}>REPERTOIRE</Link>
                 <Link to="/pricing" className={classes.link}>PRICING</Link>
                 <NavList />
